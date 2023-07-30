0e278516d0185ef82d8bd0f00831720d5994a203
lesponns <adamadakadam1903@gmail.com>
2023-07-30 06:05:58 +0300

    Add project files.

---
+++ odev_2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev_2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name,message=null;
+
+
+            for (int i = 1; i <= 10; i++) 
+            {
+                Console.Write("İsim giriniz: ");
+                name = Console.ReadLine();
+                name = i+". " + name + "\n";
+                message = message + name;
+            }
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
